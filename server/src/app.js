1d64fc8735b420b56bd32e7b386c1263dfcbb906
aimalqazi-dev <aimalqazi.work@gmail.com>
2026-04-13 18:39:38 +0500

    Create Login & Register api

--- server/src/app.js
+++ server/src/app.js
@@ -1,8 +1,11 @@
 import express from 'express';
 import cors from 'cors';
+import authRouter from './routers/auth.router.js';
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+
+app.use('/api/auth', authRouter);
 
 export default app;
---
+++ server/src/controllers/auth.controller.js
@@ -0,0 +1,56 @@
+import pool from "../db/db.js";
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+
+export const register = async (req, res) => {
+    try {
+        const { name, email, password } = req.body;
+
+        const userExists = await pool.query(
+            'SELECT * FROM users WHERE email = $1',
+            [email]
+        )
+
+        if (userExists.rows.length > 0) {
+            return res.status(400).json({ error: 'User already exists' });
+        }
+
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        const newUser = await pool.query(
+            'INSERT INTO users (name, email, password) VALUES ($1, $2, $3) RETURNING *',
+            [name, email, hashedPassword]
+        )
+
+        res.status(201).json(newUser.rows[0]);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+}
+
+export const login = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+
+        const user = await pool.query(
+            'SELECT * FROM users WHERE email = $1',
+            [email]
+        );
+
+        if (user.rows.length === 0) {
+            return res.status(400).json({ error: 'Invalid credentials' });
+        }
+
+        const isMatch = await bcrypt.compare(password, user.rows[0].password);
+
+        if (!isMatch) {
+            return res.status(400).json({ error: 'Invalid credentials' });
+        }
+
+        const token = jwt.sign({ id: user.rows[0].id }, process.env.JWT_SECRET, { expiresIn: '1h' });
+
+        res.status(200).json({ message: 'Login successful', token });
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+}
